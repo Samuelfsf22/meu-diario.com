01224bd9a7ea25cdef41c34e8047238d95908594
Samuel <samuelfsf22@gmail.com>
2025-03-24 10:06:56 -0300

    Primeiro Commit

---
+++ src/test/java/com/proj/site/diario/DiarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.site.diario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
