456d35d728b7cc12ded56c0e3219c52547e3b348
Samuel <samuelfsf22@gmail.com>
2025-04-02 13:29:50 -0300

    Linkagem

--- src/main/java/com/proj/site/diario/controller/HomeController.java
+++ src/main/java/com/proj/site/diario/controller/HomeController.java
@@ -11,4 +11,31 @@ public class HomeController {
     public String index(Model model) {
         return "index";
     }
+
+    @GetMapping("/home")
+    public String home(Model model) {return "home";}
+
+    @GetMapping("/escrever")
+    public String escreverPage(Model model) {
+        model.addAttribute("conteudo", "fragments/escrever");
+        return "index";
+    }
+
+    @GetMapping("/leitura")
+    public String lerPage(Model model) {
+        model.addAttribute("conteudo", "fragments/leitura");
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String infoPage(Model model) {
+        model.addAttribute("conteudo", "fragments/info");
+        return "index";
+    }
+
+    @GetMapping("/config")
+    public String configPage(Model model) {
+        model.addAttribute("conteudo", "fragments/config");
+        return "index";
+    }
 }
